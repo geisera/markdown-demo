6f20ccac8ba7bbd1ef8ebe3bf0358260cdf074ce
Tony <geisera@gmail.com>
2023-10-17 17:11:51 -0500

    initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,7 @@
+export function Logo(props) {
+  return (
+    <svg viewBox="0 0 150 24" aria-hidden="true" {...props}>
+      <text className="fill-zinc-900 dark:fill-white font-bold" x="20" y="15">Markdown Demo</text>
+    </svg>
+  )
+}
